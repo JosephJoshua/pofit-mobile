0f8bb1f897606cde68588aa6f60ce67d4824dea1
JosephJoshua <jj.anggita@gmail.com>
2023-04-17 11:52:15 +0700

    chore: setup project & expo-router

---
+++ src/app/_layout.tsx
@@ -0,0 +1,11 @@
+import { Stack } from 'expo-router';
+
+export const unstable_settings = {
+  initialRouteName: 'index',
+};
+
+const RootLayout = () => {
+  return <Stack screenOptions={{}} />;
+};
+
+export default RootLayout;
---
+++ src/app/index.tsx
@@ -0,0 +1,11 @@
+import { Text, View } from 'react-native';
+
+const HomePage = () => {
+  return (
+    <View style={{ flex: 1 }}>
+      <Text>test</Text>
+    </View>
+  );
+};
+
+export default HomePage;
